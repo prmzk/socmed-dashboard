d290709ea8491b731659e42a768156fd5f5d32d4
Adam Primarizki <adam.primarizki@gmail.com>
2024-04-20 23:44:08 +0700

    add router file

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,43 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import UserDetail from "./pages/UserDetail";
+import UserPosts from "./pages/UserPosts";
+import PostDetail from "./pages/PostDetail";
+import UserAlbums from "./pages/UserAlbums";
+import AlbumDetail from "./pages/AlbumDetail";
+import PhotoDetail from "./pages/PhotoDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home />,
+  },
+  {
+    path: "/:userId",
+    element: <UserDetail />,
+    children: [
+      {
+        path: "posts",
+        element: <UserPosts />,
+      },
+
+      {
+        path: "posts/:postId",
+        element: <PostDetail />,
+      },
+      {
+        path: "albums",
+        element: <UserAlbums />,
+      },
+      {
+        path: "albums/:albumId",
+        element: <AlbumDetail />,
+      },
+      {
+        path: "albums/:albumId/:photoId",
+        element: <PhotoDetail />,
+      },
+    ],
   },
 
   {
