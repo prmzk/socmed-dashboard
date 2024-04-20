0ed62da328d6238c1eb6d941745ae80aef37709c
Adam Primarizki <adam.primarizki@gmail.com>
2024-04-20 23:43:23 +0700

    add types

---
+++ src/types.ts
@@ -0,0 +1,45 @@
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+};
+
+export type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+export type Comment = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+};
+
+export type Album = {
+  userId: number;
+  id: number;
+  title: string;
+};
+
+export type Photo = {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+};
