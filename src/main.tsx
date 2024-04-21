67476ecfa320c4f5d896430758e0d6d113ada3a9
Adam Primarizki <adam.primarizki@gmail.com>
2024-04-21 12:29:32 +0700

    add tailwind and nextui

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import router from "./router.tsx";
+import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <main>
-      <RouterProvider router={router} />
-    </main>
+    <NextUIProvider>
+      <main className="bg-slate-700 min-h-svh">
+        <div className="container px-4 mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </main>
+    </NextUIProvider>
   </React.StrictMode>
 );
