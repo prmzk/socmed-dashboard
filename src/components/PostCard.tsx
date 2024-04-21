cfd108791cb301a443c4431ab2efaa77737c6b0e
Adam Primarizki <adam.primarizki@gmail.com>
2024-04-21 14:29:10 +0700

    add comment and post delet edit and add feature

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,24 +1,99 @@
-import { Button } from "@nextui-org/react";
+import { Button, Input, Textarea } from "@nextui-org/react";
 import { Link } from "react-router-dom";
 import { Post } from "../types";
+import { useState } from "react";
 
 type Props = {
   post: Post;
+  deletePost: (postIdToDelete: number) => Promise<void>;
 };
 
-const PostCard = ({ post }: Props) => {
+const PostCard = ({ post, deletePost }: Props) => {
+  const [title, setTitle] = useState(post.title);
+  const [body, setBody] = useState(post.body);
+  const [isEdit, setIsEdit] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const editPost = async () => {
+    setLoading(true);
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${post.id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          title,
+          body,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+    await response.json();
+    setLoading(false);
+    setIsEdit(false);
+  };
+
+  const submitDelete = async () => {
+    setLoading(true);
+    await deletePost(post.id);
+    setLoading(false);
+  };
+
   return (
     <div className="flex border-b border-gray-300 py-8">
       <div className="flex flex-col">
-        <div className="flex gap-2 items-center flex-wrap">
-          <p className="text-xl font-bold text-gray-200">{post.title}</p>
-          <p className="text-md text-gray-200">{post.body}</p>
-        </div>
-        <div className="flex mt-2">
-          <Button color="default" as={Link} to={`${post.id}`}>
-            Check Comment
-          </Button>
-        </div>
+        {!isEdit ? (
+          <div className="flex flex-col gap-2 flex-wrap">
+            <p className="text-xl font-bold text-gray-200">{title}</p>
+            <p className="text-md text-gray-200 whitespace-pre-line">{body}</p>
+          </div>
+        ) : (
+          <div className="flex gap-2 items-center flex-wrap">
+            <Input
+              type="text"
+              label="Title"
+              className="mt-2"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
+            <Textarea
+              type="text"
+              label="Post"
+              className="mt-2"
+              onChange={(e) => setBody(e.target.value)}
+              value={body}
+            />
+          </div>
+        )}
+        {!isEdit ? (
+          <div className="flex mt-2 gap-3">
+            <Button
+              color="default"
+              as={Link}
+              to={`${post.id}`}
+              disabled={loading}
+            >
+              Check Comment
+            </Button>
+            <Button
+              color="default"
+              onClick={() => setIsEdit(true)}
+              disabled={loading}
+            >
+              Edit Post
+            </Button>
+            <Button color="danger" onClick={submitDelete} disabled={loading}>
+              {loading ? "Loading..." : "Delete Post"}
+            </Button>
+          </div>
+        ) : (
+          <div className="flex mt-2 gap-3">
+            <Button color="default" onClick={editPost} disabled={loading}>
+              {loading ? "Loading..." : "Submit"}
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/UserPosts.tsx
+++ src/pages/UserPosts.tsx
@@ -3,17 +3,63 @@ import useSWR from "swr";
 import PostCard from "../components/PostCard";
 import fetcher from "../config/fetcher";
 import { Post } from "../types";
-import { Spinner } from "@nextui-org/react";
+import { Button, Spinner } from "@nextui-org/react";
+import { Input, Textarea } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
 const UserPosts = () => {
   const { userId } = useParams<{ userId: string }>();
+  const [postData, setPostData] = useState<Post[]>([]);
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [additionPost, setAdditionPost] = useState<Post[]>([]);
 
   const { data, isLoading } = useSWR(
     `https://jsonplaceholder.typicode.com/users/${userId}/posts`,
     fetcher
   );
 
-  const postData: Post[] = data;
+  const submitPost = async () => {
+    setLoading(true);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        body,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const responseJson: Post = await response.json();
+    setAdditionPost((prev: Post[]) => [responseJson, ...prev]);
+    setLoading(false);
+    setTitle("");
+    setBody("");
+  };
+
+  const deletePost = async (postIdToDelete: number) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+    await response.json();
+    setPostData((prevData) =>
+      prevData.filter((post) => post.id !== postIdToDelete)
+    );
+  };
+
+  useEffect(() => {
+    setPostData(data);
+  }, [data]);
 
   if (isLoading) return <Spinner className="w-20 h-20" />;
   if (!postData || !postData[0]) return <div>no data</div>;
@@ -21,10 +67,32 @@ const UserPosts = () => {
   // render data
   return (
     <div>
-      <h3 className="text-2xl text-gray-300 mt-8">Post List</h3>
+      <h3 className="text-2xl text-gray-300 mt-8 mb-4">Post List</h3>
+
+      <div>
+        <h4 className="font-bold text-gray-400">Add Post</h4>
+        <Input
+          type="text"
+          label="Title"
+          className="mt-2"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <Textarea
+          type="text"
+          label="Post"
+          className="mt-2"
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+        />
+        <Button className="mt-4" onClick={submitPost} disabled={loading}>
+          {loading ? "Loading..." : "Submit"}
+        </Button>
+      </div>
+
       <div className="grid py-8">
-        {postData?.map((post) => (
-          <PostCard key={post.id} post={post} />
+        {[...additionPost, ...postData]?.map((post) => (
+          <PostCard key={post.id} post={post} deletePost={deletePost} />
         ))}
       </div>
     </div>
