183e4f154516b1738a480e36bf3b9d1d8cb83059
Adam Primarizki <adam.primarizki@gmail.com>
2024-04-21 13:27:15 +0700

    add main title

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <NextUIProvider>
       <main className="bg-slate-700 min-h-svh">
         <div className="container px-4 mx-auto">
+          <h1 className="text-white text-xl font-bold py-8 border-b border-white">
+            Social Media Dashboard
+          </h1>
           <RouterProvider router={router} />
         </div>
       </main>
